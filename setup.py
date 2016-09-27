a226ed293182c06e92485e347423d86deb185bb1
suraj <srjkumarxxx@gmail.com>
2016-09-27 15:20:16 +0530

    first Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DockerPy',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/srjkumar/DockerPy',
+    license='',
+    author='suraj',
+    author_email='srjkumarxxx@gamil.com',
+    description='Under Devloping Script'
+)
